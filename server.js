83cce31597ecc769d269d5a007696b1d090f4ddd
ffakira <55924290+ffakira@users.noreply.github.com>
2022-12-18 02:19:58 +0800

    impl: graceful shutdown server

--- server.js
+++ server.js
@@ -60,6 +60,22 @@ require('./services/lyric')(app, pool)
 
 /** @dev start server */
 const PORT = process.env.PORT || 4000
-app.listen(PORT, async () => {
-  console.log('[console] Listerning to port:', PORT)
+const server = app.listen(PORT, () => console.log('[console] Listening to port:', PORT))
+
+/** @dev gracefully shutdown server */
+process.on('SIGINT', () => {
+  console.log('\n[console] Requested close server on port:', PORT)
+
+  server.close(async () => {
+    console.log('[console] Closing server on port:', PORT)
+    try {
+      await pool.end()
+      console.log('[console] Successfully closed pool connections from postgres')
+      console.log('[console] Successfully closed port:', PORT)
+      process.exit(0)
+    } catch (err) {
+      console.error('[console] Error occured closing postgres:', err.message)
+      process.exit(1)
+    }
+  })
 })
