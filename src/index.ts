5a41725a99000a746bcb2e85585db56514d5cb7b
ffakira <55924290+ffakira@users.noreply.github.com>
2022-12-25 07:47:53 +0800

    temp(test): removed session temp

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 require('dotenv').config()
 import express from 'express'
-import session, { MemoryStore, Store } from 'express-session'
+// import session, { MemoryStore, Store } from 'express-session'
 import morgan from 'morgan'
 import cors from 'cors'
-import { ONE_DAY } from './utils'
+// import { ONE_DAY } from './utils'
 import router from './router'
-import PgStore from './utils/store'
+// import PgStore from './utils/store'
 const app = express()
 const { NODE_ENV } = process.env
 
-const storeStrategy = NODE_ENV === 'test' ? new MemoryStore() : new PgStore()
-const secure = NODE_ENV === 'development' || NODE_ENV === 'test' ? false : true
-app.use(
-  session({
-    store: storeStrategy as Store,
-    name: process.env.COOKIE_NAME,
-    secret: process.env.COOKIE_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      path: '/',
-      secure,
-      maxAge: ONE_DAY,
-      httpOnly: true
-    }
-  })
-)
+// const storeStrategy = NODE_ENV === 'test' ? new MemoryStore() : new PgStore()
+// const secure = NODE_ENV === 'development' || NODE_ENV === 'test' ? false : true
+// app.use(
+//   session({
+//     store: storeStrategy as Store,
+//     name: process.env.COOKIE_NAME,
+//     secret: process.env.COOKIE_SECRET as string,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       path: '/',
+//       secure,
+//       maxAge: ONE_DAY,
+//       httpOnly: true
+//     }
+//   })
+// )
 
 /** @dev addtional middlewares */
 const morganEnv = NODE_ENV === 'development' ? 'dev' : 'combined'
